89023f8bd8085a217f451d30bc39241ce90f087d
Tyler Rayls <62755797+Tyler-Rayls@users.noreply.github.com>
2021-05-03 17:04:15 -0400

    Reused numArray to improve space complexity

--- main.js
+++ main.js
@@ -11,15 +11,14 @@ digitToStringMap["8"] = "Eight";
 digitToStringMap["9"] = "Nine";
 
 const numsToString = (numArray) => {
-    phoneticNums = [];
     for (let i = 0; i < numArray.length; i ++) {
         numArray[i] = numArray[i].toString();
-        phoneticNums.push("");
-        for (let j = 0; j < numArray[i].length; j++) {
-            phoneticNums[i] += digitToStringMap[numArray[i][j]];
+        numLength = numArray[i].length;
+         for (let j = 0; j < numLength; j++) {
+             numArray[i] = numArray[i].slice(1) + digitToStringMap[numArray[i][0]];
         }
     }
-    return phoneticNums;
+    return numArray;
 }
 
 numArray = process.argv.slice(2);
