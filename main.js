13448ceb48e65b22d14469848979539731f724d8
Tyler Rayls <62755797+Tyler-Rayls@users.noreply.github.com>
2021-05-03 17:01:46 -0400

    Function written and working

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+digitToStringMap = new Map();
+digitToStringMap["0"] = "Zero";
+digitToStringMap["1"] = "One";
+digitToStringMap["2"] = "Two";
+digitToStringMap["3"] = "Three";
+digitToStringMap["4"] = "Four";
+digitToStringMap["5"] = "Five";
+digitToStringMap["6"] = "Six";
+digitToStringMap["7"] = "Seven";
+digitToStringMap["8"] = "Eight";
+digitToStringMap["9"] = "Nine";
 
+const numsToString = (numArray) => {
+    phoneticNums = [];
+    for (let i = 0; i < numArray.length; i ++) {
+        numArray[i] = numArray[i].toString();
+        phoneticNums.push("");
+        for (let j = 0; j < numArray[i].length; j++) {
+            phoneticNums[i] += digitToStringMap[numArray[i][j]];
+        }
+    }
+    return phoneticNums;
+}
+
+let arr1 = [2, 300, 56, 74, 9];
+console.log(numsToString(arr1));
